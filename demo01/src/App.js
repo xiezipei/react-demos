689b114ce3c75cae76fc3ceeac7c52799bad1ed2
xiezipei <xiezipei@zckd>
2021-05-11 15:33:41 +0800

    update(demo01): 练习 05

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
 
+/** 
+ * 上面是es6解构赋值写法，等价于
+ */
+// import React from 'react'
+// const Component = React.Component
+
 class App extends Component {
     render() {
         return (
-            <div>Hello World</div>
+            <div>Hello World!</div>
         )
     }
 }
--- demo01/src/index.js
+++ demo01/src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// 把 `<App>` 组件挂载到模板 `root` 节点上
 ReactDOM.render(<App />, document.getElementById('root'))

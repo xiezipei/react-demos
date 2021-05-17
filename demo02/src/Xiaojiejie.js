53b2ac34fe8b065dcc32c5109bffc4b5437ac154
xiezipei <xiezipei@zckd>
2021-05-17 15:28:49 +0800

    update(demo02): 响应式设计和数据的绑定

--- demo02/src/Xiaojiejie.js
+++ demo02/src/Xiaojiejie.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from 'react'
 
 class Xiaojiejie extends Component {
+    // JS的构造函数，由其他函数执行
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: '', // input中的值
+            list: []    // 服务列表
+        }
+    }
     render() {
         return (
             <Fragment>
                 <div>
-                    <input placeholder="请输入" /><button>增加服务</button>
+                    <input placeholder="请输入" value={ this.state.inputValue } /><button>增加服务</button>
                 </div>
                 <ul>
                     <li>头部按摩</li>

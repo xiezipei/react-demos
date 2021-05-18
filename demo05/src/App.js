10b7f3198875a9a093041a1bd23b7a93d416e417
xiezipei <xiezipei@zckd>
2021-05-18 17:22:25 +0800

    feat(demo05): 调用 easy-mock 获取并渲染数据

--- demo05/src/App.js
+++ demo05/src/App.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-    state = {  }
+    state = { 
+        list: []
+     }
 
     componentDidMount() {
-        axios.get('http://book.douban.com/review/1000104/')
-            .then(res => console.log('success', res))
+        axios.get('https://easy-mock.bookset.io/mock/60a3753447ef9e51d0ad2a87/react-demo/list/get')
+            .then(res => {
+                console.log('success', res);
+                this.setState({
+                    list: res.data.data
+                })
+                console.log(this.state.list);
+            })
             .catch(err => console.log('fail', err));
     }
 
     render() { 
-        return (<div>Mock Demo</div>);
+        return (
+            <div>
+                <h1>Mock Demo</h1>
+                <ul>
+                    { this.state.list.map((item, index) => <li key={ index }>{ item }</li>) }
+                </ul>
+            </div>
+        );
     }
 }
  

2ace9c77837016cfaada317afb90f57aad36d226
xiezipei <xiezipei@zckd>
2021-05-19 10:22:06 +0800

    feat(demo06): react-transition-group

--- demo06/src/Boss.js
+++ demo06/src/Boss.js
@@ -1,21 +1,38 @@
 import React, { Component, Fragment } from 'react';
 import './css/boss.css'
+import { CSSTransition } from 'react-transition-group'
 
 class Boss extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isShow: false
+            isShow: true
         }
         this.toToggle = this.toToggle.bind(this);
     }
-    render() { 
+
+    // render() { 
+    //     return (
+    //         <Fragment>
+    //             <div className={ this.state.isShow ? 'show' : 'hide' }>Boss级人物 - 孙悟空</div>
+    //             <div><button onClick={ this.toToggle }>{ this.state.isShow ? '隐藏Boss' : '召唤Boss' }</button></div>
+    //         </Fragment>
+    //     );
+    // }
+
+    render() {
         return (
             <Fragment>
-                <div className={ this.state.isShow ? 'show' : 'hide' }>Boss级人物 - 孙悟空</div>
+                <CSSTransition
+                    in={ this.state.isShow }
+                    timeout= { 2000 }
+                    classNames="boss-text"
+                >
+                    <div>Boss级人物-孙悟空</div>
+                </CSSTransition>
                 <div><button onClick={ this.toToggle }>{ this.state.isShow ? '隐藏Boss' : '召唤Boss' }</button></div>
             </Fragment>
-        );
+        )
     }
 
     toToggle() {

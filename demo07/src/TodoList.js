8fe328f8fdf33e0fd512bb3a8b33756fe744ddfd
xiezipei <xiezipei@gmail.com>
2021-05-26 15:15:50 +0800

    feat(demo07): 通过 input 理解 redux 处理数据流程

--- demo07/src/TodoList.js
+++ demo07/src/TodoList.js
@@ -3,23 +3,44 @@ import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store/index'
 
-// const data = [
-//     'do something 1',
-//     'do something 2',
-//     'do something 3',
-// ]
-
 class TodoList extends Component {
     constructor(props) {
         super(props)
+
+        // 获取 `state` 对象
         this.state = store.getState()
-        console.log(this.state)
+
+        // 绑定输入框值变化方法
+        this.changeInputValue = this.changeInputValue.bind(this)
+
+        // 转变this指向
+        this.storeChange = this.storeChange.bind(this)
+
+        // 订阅 redux 状态
+        store.subscribe(this.storeChange)
+    }
+    storeChange(){
+        console.log(3, 'storeChange');
+        // 用了组件的 setState 方法
+        this.setState(store.getState())
+    }
+    changeInputValue(e) {
+        console.log(1, 'changeInputValue');
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action); // 通过 `dispatch()` 方法把数据传递给 `store`
     }
     render() {
         return (
             <div style={{ margin: '24px' }}>
                 <div>
-                    <Input placeholder={ this.state.inputValue } style={{ width: '250px', marginRight: '10px' }} />
+                    <Input 
+                        placeholder={ this.state.inputValue }
+                        style={{ width: '250px', marginRight: '10px' }} 
+                        onChange={ this.changeInputValue }
+                    />
                     <Button type="primary">增加</Button>
                 </div>
                 <div style={{ marginTop: '10px', width: '324px' }}>
--- demo07/src/store/reducer.js
+++ demo07/src/store/reducer.js
@@ -7,6 +7,24 @@ const defaultState = {  //默认数据
     ]
 }
 
+/**
+ * store只是一个仓库，并没有管理能力，
+ * 它会把接收到的 action 自动转发给 reducer。
+ * 
+ * 这里有两个参数：
+ * - state：指的是原始仓库里的状态
+ * - action：指的是 action 新传递的状态
+ * 
+ * 要记住：reducer 里只能接收 state，不能改变 state
+ */
 export default (state = defaultState, action)=>{  //就是一个方法函数
+    console.log(2, 'reducer');
+    // 如果类型正确，传递新状态
+    if (action.type === 'changeInput') {
+        let newState = JSON.parse(JSON.stringify(state))    // 深拷贝
+        newState.InputValue = action.InputValue
+        return newState
+    }
+    // 否则，传递原来状态
     return state
 }

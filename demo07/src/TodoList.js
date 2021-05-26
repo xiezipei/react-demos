5e0b0a2f3b0f01e00474d064e858458fc111cb6a
xiezipei <xiezipei@gmail.com>
2021-05-26 11:48:58 +0800

    feat(demo07): 使用 antd

--- demo07/src/TodoList.js
+++ demo07/src/TodoList.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
-import { Button, Input } from 'antd'
+import { Input, Button, List } from 'antd'
+
+const data = [
+    'do something 1',
+    'do something 2',
+    'do something 3',
+    'do something 4',
+    'do something 5',
+]
 
 class TodoList extends Component {
-    state = {  }
-    render() { 
+    render() {
         return (
-            <div style={{ padding: '24px' }}>
+            <div style={{ margin: '24px' }}>
                 <div>
-                    <Input placeholder="write something" style={{ width: '200px' }} />
+                    <Input placeholder='write someting' style={{ width: '250px', marginRight: '10px' }} />
                     <Button type="primary">增加</Button>
+                </div>
+                <div style={{ marginTop: '10px', width: '324px' }}>
+                    <List
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
                 </div>
             </div>
         );
     }
 }
- 
 export default TodoList;

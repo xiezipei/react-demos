a4f4af6b94a299a0d4410567e15ee0f3eeb911fc
xiezipei <xiezipei@gmail.com>
2021-06-02 14:47:20 +0800

    feat(demo08): 初始化项目 & 引入 react-redux

---
+++ demo08/src/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (<h1>helloworld</h1>);
+    }
+}
+ 
+export default TodoList;

c2c0d95e0473d8dd7998fc4e1579cf9e2022b51c
xiezipei <xiezipei@gmail.com>
2021-06-02 11:10:48 +0800

    update(demo07): 使用 redux-thunk

--- demo07/src/store/index.js
+++ demo07/src/store/index.js
@@ -3,13 +3,7 @@ import reducer from './reducer'
 import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-
 const enhancer = composeEnhancers(applyMiddleware(thunk))
-
-const store = createStore(
-    reducer,
-    enhancer
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, enhancer);
 
 export default store;

b30187e7aba8848e5bf8d4dd6fd59592820718f2
xiezipei <xiezipei@gmail.com>
2021-06-02 11:34:14 +0800

    update(demo07): 引入并配置 redux-saga

--- demo07/src/store/index.js
+++ demo07/src/store/index.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+const sagaMiddleware = createSagaMiddleware()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-const enhancer = composeEnhancers(applyMiddleware(thunk))
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
+
 const store = createStore(reducer, enhancer);
 
 export default store;

d14ae56d6a20a4ee99a05031edc27e5ef3c4a1b3
xiezipei <xiezipei@gmail.com>
2021-06-03 14:58:00 +0800

    update(demo10): 动态路由模拟

--- demo10/src/AppRouter.js
+++ demo10/src/AppRouter.js
@@ -5,6 +5,13 @@ import Video from './pages/Video'
 import Workplace from './pages/Workplace'
 import './index.css'
 
+// 路由配置
+const routeConfig = [
+    { path: '/', title: '首页', exact: true, component: Index },
+    { path: '/video', title: '视频教程', exact: true, component: Video },
+    { path: '/workplace', title: '工作空间', exact: true, component: Workplace },
+]
+
 function AppRouter() {
     return (
         <Router>
@@ -12,15 +19,25 @@ function AppRouter() {
                 <div className="leftNav">
                     <h3>一级导航</h3>
                     <ul>
-                        <li><Link to="/">首页</Link></li>
+                        {/* <li><Link to="/">首页</Link></li>
                         <li><Link to="/video">视频教程</Link></li>
-                        <li><Link to="/workplace">工作空间</Link></li>
+                        <li><Link to="/workplace">工作空间</Link></li> */}
+                        {
+                            routeConfig.map((item, index) => {
+                                return (<li key={ index} ><Link to={ item.path }>{ item.title }</Link></li>)
+                            })
+                        }
                     </ul>
                 </div>
                 <div className="rightMain">
-                    <Route path="/" exact component={ Index } />
+                    {/* <Route path="/" exact component={ Index } />
                     <Route path="/video" component={ Video } />
-                    <Route path="/workplace" component={ Workplace } />
+                    <Route path="/workplace" component={ Workplace } /> */}
+                    {
+                        routeConfig.map((item, index) => {
+                            return (<Route path={ item.path } component={ item.component } />)
+                        })
+                    }
                 </div>
             </div>
         </Router>

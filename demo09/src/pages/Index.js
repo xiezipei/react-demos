a941d505d0b7c6f979dc7f9945bddde9938e126c
xiezipei <xiezipei@gmail.com>
2021-06-03 12:13:27 +0800

    update(demo09): 列表跳转，路由动态传值

--- demo09/src/pages/Index.js
+++ demo09/src/pages/Index.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 class Index extends Component {
-    state = {  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: [
+                { cid: 124, title: '123' },
+                { cid: 456, title: '456' },
+                { cid: 789, title: '789' },
+            ]
+        }
+    }
     render() { 
         return (
-            <h1>Index</h1>
+            <div>
+                <h1>Index</h1>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li key={ index }>
+                                    <Link to={`/list/${item.cid}`}>{ item.title }</Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         );
     }
 }

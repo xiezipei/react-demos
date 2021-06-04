438957f57d0d16120dfb23bc1af458cc225aac9c
xiezipei <zipeixie@gmail.com>
2021-06-04 15:03:43 +0800

    update(demo11): useWinSize 添加注释

--- demo11/src/AppCustomerHooks/useWinSize.js
+++ demo11/src/AppCustomerHooks/useWinSize.js
@@ -1,11 +1,13 @@
 import { useState, useEffect, useCallback } from 'react'
 
 function useWinSize() {
+    // 定义一个size，一个设置size的方法
     const [size, setSize] = useState({
-        width: document.documentElement.clientWidth,
+        width: document.documentElement.clientWidth,    // 设置初始值
         height: document.documentElement.clientHeight,
     })
 
+    // 使用useCallback缓存方法（useMemo是缓存变量）
     const onResize = useCallback(() => {
         setSize({
             width: document.documentElement.clientWidth,
@@ -13,8 +15,11 @@ function useWinSize() {
         })
     }, [])
 
+    // 视图每次渲染时会执行useEffect
     useEffect(() => {
+        // 监听窗体resize事件
         window.addEventListener('resize', onResize)
+        // 防止一直监听
         return () => {
             window.removeEventListener('resize', onResize)
         }

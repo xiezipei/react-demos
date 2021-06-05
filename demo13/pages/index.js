c7e6331ed6d88bc8ba5cd685b8d0f1a5fbe331a6
xiezipei <zipeixie@gmail.com>
2021-06-05 16:15:45 +0800

    update(demo13): 路由-编程式带参数跳转

--- demo13/pages/index.js
+++ demo13/pages/index.js
@@ -5,16 +5,22 @@ const Home = () => {
   function gotoPageA() {
     Router.push('/pageA')
   }
+
+  function gotoPageC() {
+    Router.push('/pageC?name=456')
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <ul>
         <li><Link href="/pageA"><a>Go to Page A.</a></Link></li>
         <li><Link href="/pageB"><a>Go to Page B.</a></Link></li>
-        <li><Link href="/pageC?name=123"><a>Go to Page C with name.</a></Link></li>
+        <li><Link href="/pageC?name=123"><a>Go to Page C with 123.</a></Link></li>
       </ul>
       <div>
         <button onClick={gotoPageA}>跳转到PageA</button>
+        <button onClick={gotoPageC}>跳转到PageC with 456</button>
       </div>
     </div>
   )

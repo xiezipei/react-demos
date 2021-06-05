ac7199a5b0e53ecc03f6c14d11bd15546723f38d
xiezipei <zipeixie@gmail.com>
2021-06-05 11:44:46 +0800

    update(demo13): pages

---
+++ demo13/pages/blog/greet.js
@@ -0,0 +1,7 @@
+// function Greet() {
+//     return (<h1>你好。</h1>)
+// }
+
+// export default Greet
+
+export default () => (<h1>你好。</h1>)
---
+++ demo13/pages/helloworld.js
@@ -0,0 +1,8 @@
+// function Helloworld() {
+//     return (<h1>Hello world!</h1>)
+// }
+
+// export default Helloworld
+
+
+export default () => (<h1>Hello world!</h1>)
